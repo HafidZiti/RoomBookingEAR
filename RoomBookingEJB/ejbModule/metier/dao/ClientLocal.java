02644ed92b07708bb384614b661c6e97f39663bb
Mehdi Azzabi <mehdi.azzabi.08@gmail.com>
2017-11-16 18:02:28 +0100

    add doLogin service

--- RoomBookingEJB/ejbModule/metier/dao/ClientLocal.java
+++ RoomBookingEJB/ejbModule/metier/dao/ClientLocal.java
@@ -11,6 +11,7 @@ public interface ClientLocal {
 	
 	public Client addClient(Client L);
 	public Client getClient(int id);
+	public Client getClient(String mail , String mdp);
 	public List<Client> getAllClient();
 	public Client updateClient(int id, String adresse, String email,String telephone, String nom,
 								   String prenom);
