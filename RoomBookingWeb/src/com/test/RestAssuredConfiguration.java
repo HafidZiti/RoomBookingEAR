70a7e24fc5ebd93a8aaab27e7c3ca4fdb1b083e4
chiraz29 <limayemchiraz7@gmail.com>
2017-12-21 14:07:55 +0100

    commit eliminate duplicate methode (test rest assured Configuration)

--- RoomBookingWeb/src/com/test/RestAssuredConfiguration.java
+++ RoomBookingWeb/src/com/test/RestAssuredConfiguration.java
@@ -12,31 +12,6 @@ import com.jayway.restassured.specification.RequestSpecification;
 
 class RestAssuredConfiguration {
 
-	@BeforeClass   
-	public static void setup() {
-        String port = System.getProperty("server.port");
-        if (port == null) {
-            RestAssured.port = Integer.valueOf(8080);
-        }
-        else{
-            RestAssured.port = Integer.valueOf(port);
-        }
-
-
-        String basePath = System.getProperty("server.base");
-        if(basePath==null){
-            basePath = "/RoomBookingWeb/";
-        }
-        RestAssured.basePath = basePath;
-
-        String baseHost = System.getProperty("server.host");
-        if(baseHost==null){
-            baseHost = "http://localhost";
-        }
-        RestAssured.baseURI = baseHost;
-
-}
-
 	public RequestSpecification getRequestSpecification() {
 		// TODO Auto-generated method stub
 		return RestAssured.given().contentType(ContentType.JSON);
